5a2bc3b0953af43d69d742d533c06585fea2f7fe
itsferdiardiansa <ferdiardiansa@gmail.com>
2024-11-03 22:48:56 +0700

    docs: updated readme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,9 +41,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" /> */}
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
